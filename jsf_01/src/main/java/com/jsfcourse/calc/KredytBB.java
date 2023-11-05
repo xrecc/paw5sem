dac516bbe9ab38e4bf87a8f14618b031280a4b1b
xrecc <sebakaktus07@gmail.com>
2023-11-05 18:51:17 +0100

    Poprawki

--- jsf_01/src/main/java/com/jsfcourse/calc/KredytBB.java
+++ jsf_01/src/main/java/com/jsfcourse/calc/KredytBB.java
@@ -48,34 +48,23 @@ public class KredytBB {
 		this.result = result;
 	}
 
-	public boolean doTheMath() {
+	public String calc() {
 		try {
 			double kwota = Double.parseDouble(this.kwota);
 			double czas = Double.parseDouble(this.czas);
 			double procent = Double.parseDouble(this.procent);
 			result = (double) Math.round(((kwota/(czas*12))+(kwota/(czas*12))*(procent/100))*100)/100;
 
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
-			return true; 
-		} catch (Exception e) {
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
-			return false; 
-		}
-	}
-	public String calc() {
-		if (doTheMath()) {
-			return "showresult";
-		}
-		return null;
-	}
-	public String calc_AJAX() {
-		if (doTheMath()) {
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Rata miesięczna: " + result, null));
-		}
-		return null;
-	}
+			 ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+	            return "showresult"; 
+	        } catch (Exception e) {
+	            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+	            return null; 
+	        }
 
-	public String info() {
-		return "info";
+	    }
+
+	    public String info() {
+	        return "info"; 
+	    }
 	}
-}
